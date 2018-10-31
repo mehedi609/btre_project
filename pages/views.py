afed9b0bba4fd1d565ad09eae7877e9a59301f16
mehedi609 <mehedi609@gmail.com>
2018-11-01 02:18:11 +0600

    static commit

--- pages/views.py
+++ pages/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello world')
+    return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
