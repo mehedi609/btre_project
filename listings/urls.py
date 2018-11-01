4673ce3c94a7c7982a620acdbac8756b0d740847
mehedi609 <mehedi609@gmail.com>
2018-11-01 10:43:44 +0600

    asdf

--- listings/urls.py
+++ listings/urls.py
@@ -17,7 +17,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='listings'),
     path('<int:listing_id>', views.listing, name='listing'),
     path('search', views.search, name='search')
 ]
