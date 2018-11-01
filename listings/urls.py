03686d5ee17993d7a6e43064130448d005b050e3
mehedi609 <mehedi609@gmail.com>
2018-11-01 10:30:29 +0600

    asd

--- listings/urls.py
+++ listings/urls.py
@@ -18,5 +18,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about')
+    path('<int:listing_id>', views.listing, name='listing'),
+    path('search', views.search, name='search')
 ]
--- realtors/urls.py
+++ realtors/urls.py
@@ -18,5 +18,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about')
+    path('<int:listing_id>', views.li, name='about')
 ]
